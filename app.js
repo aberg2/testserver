5129ce2d1c14515736540e90d50a9aced00ee4cf
emil aberg <emil.aberg@elev.ga.ntig.se>
2022-02-25 11:16:22 +0100

    yo

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const sassMiddleware = require('node-sass-middleware');
+const nunjucks = require('nunjucks')
+
+require('dotenv').config();
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const tasksRouter = require('./routes/tasks');
+const tasksApiRouter = require('./routes/api/tasks');
+
+const app = express();
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app
+});
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(sassMiddleware({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  indentedSyntax: false, // true = .sass and false = .scss
+  sourceMap: true
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/tasks', tasksRouter);
+app.use('/api/tasks', tasksApiRouter);
+
+module.exports = app;
---
+++ routes/tasks.js
@@ -0,0 +1,137 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../database');
+/* 
+    BASE URL /tasks
+    GET / - Get all tasks
+    POST / - Create a new task
+    GET /:id - Get a task by id
+    PUT /:id - Update a task by id
+    DELETE /:id - Delete a task by id
+*/
+router.get('/', async (req, res, next) => {
+    await pool.promise()
+        .query('SELECT * FROM tasks')
+        .then(([rows, fields]) => {
+              res.render('tasks.njk', {
+                tasks: rows,
+                title: 'Tasks',
+                layout: 'layout.njk'
+              });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                tasks: {
+                    error: 'Error getting tasks'
+                }
+            })
+        });
+});
+
+router.get('/:id', async (req, res, next) => {
+    const id = req.params.id;
+    if (isNaN(req.params.id)) {
+        res.status(400).json({
+            task: {
+                error: 'Bad request'
+            }
+        });
+    }
+    await pool.promise()
+        .query('SELECT * FROM tasks WHERE id = ?', [id])
+        .then(([rows, fields]) => {
+            res.json({
+                task: {
+                    data: rows
+                }
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                task: {
+                    error: 'Error getting tasks'
+                }
+            })
+        });
+});
+
+router.get('/:id/delete', async (req, res, next) => {
+    const id = req.params.id;
+    res.json(`deleting task ${id}`);
+    // if (isNaN(req.params.id)) {
+    //     res.status(400).json({
+    //         task: {
+    //             error: 'Bad request'
+    //         }
+    //     });
+    // }
+});
+
+router.post('/', async (req, res, next) => {
+    // { "task": "koda post" }
+    const task = req.body.task;
+        if(task.length <3) {
+            res.status(400).json({
+                task: {
+                    error: "A task must have at least 3 characters"
+                }
+            })
+        }
+
+    await pool.promise()
+    .query('INSERT INTO tasks (task) VALUES (?)', [task])
+    .then((response) => {
+        console.log(response[0].affectedRows);
+        if (response[0].affectedRows == 1) {
+
+            res.redirect('/tasks');
+        }else{
+            res.status(500).json({message: "det funkade inte!"});
+        }
+
+        /*
+        res.json({
+            task: {
+                data: response
+            }
+        });*/
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            task: {
+                error: 'Error getting tasks'
+            }
+        })
+    });
+    
+    
+    // res.json(req.body);
+
+});
+
+
+module.exports = router;
+
+
+
+/*
+
+    await pool
+    .promise()
+    .query('SELECT * FROM users')
+    .then(([rows, fields]) => {
+        res.json({
+            data: rows,
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json({
+            error: 'Database error',
+        });
+    });
+
+    */
